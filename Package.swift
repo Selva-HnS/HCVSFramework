c3abc8ddc8497c6a3047f5b8f790d93ba70f0e55
arunpandian.c <arunpandianc@hnsonline.com>
2025-03-21 15:01:42 +0530

    first commit

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "HCVSFramework",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "HCVSFramework",
+            targets: ["HCVSFramework"]
+        ),
+    ],
+    dependencies: [
+        // Add any other dependencies here
+    ],
+    targets: [
+        .binaryTarget(
+            name: "CVSInference",
+            path: "./CVSInference.xcframework"
+        ),
+        .target(
+            name: "HCVSFramework",
+            dependencies: ["HCVSFramework"]
+        ),
+        .testTarget(
+            name: "MyPackageTests",
+            dependencies: ["HCVSFramework"]
+        ),
+    ]
+)
