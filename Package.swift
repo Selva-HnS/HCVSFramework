72b5d1ac66776353c1716f52c8b7ff57960f279d
arunpandian.c <arunpandianc@hnsonline.com>
2025-03-21 15:06:37 +0530

    file updated commit

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         ),
         .target(
             name: "HCVSFramework",
-            dependencies: ["HCVSFramework"]
+            dependencies: ["CVSInference"]
         ),
         .testTarget(
             name: "MyPackageTests",
